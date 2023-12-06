6dec8eec1d0e424140f8edc4effe5fd175a8e893
walter.hwang <lyvius2@naver.com>
2023-12-06 22:02:43 +0900

    Producer 초기 commit

---
+++ producer/src/test/java/com/walter/active/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.active.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
